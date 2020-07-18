90548eb59ff1b19f2e7a44912b0e77e23bad6d66
FerBarragan10 <fer7_10@hotmail.com>
2020-07-18 18:35:58 -0300

    nuevos servicios y cambio de ruta

--- frontAngular/app/src/app/services/client.service.ts
+++ frontAngular/app/src/app/services/client.service.ts
@@ -12,46 +12,45 @@ export class ClientService {
 
   getClientes(){
   
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/cliente');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/cliente');
   }
-
   getProductos(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/producto');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/producto');
   }
   getVendedores(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/vendedor');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/vendedor');
   }
 
   getTiposProductos(){
-  return this.http.get('http://localhost/apiRestSistema/apiRest/api/tipoProducto');
+  return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/tipoProducto');
 }
 
 getMarcas(){
-  return this.http.get('http://localhost/apiRestSistema/apiRest/api/marca');
+  return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/marca');
 }
   insertProducto(producto:Producto){
     console.log(producto);
-    return this.http.post('http://localhost/apiRestSistema/apiRest/api/producto',producto);
+    return this.http.post('http://localhost/ponetela10/apiRestSistema/apiRest/api/producto',producto);
   }
   agregaCliente(cliente:Cliente){
     console.log("llego aca");
     console.log(cliente);
-    return this.http.post('http://localhost/apiRestSistema/apiRest/api/cliente',cliente);
+    return this.http.post('http://localhost/ponetela10/apiRestSistema/apiRest/api/cliente',cliente);
   }
   getCamisetas(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/camisetas');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/camisetas');
   }
   getBuzos(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/buzos');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/buzos');
   }
   getCamperas(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/camperas');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/camperas');
   }
   getGorras(){
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/gorras');
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/gorras');
   }
   getProducto(id:number){
     console.log(id);
-    return this.http.get('http://localhost/apiRestSistema/apiRest/api/producto/'+id);
+    return this.http.get('http://localhost/ponetela10/apiRestSistema/apiRest/api/producto/'+id);
   }
 }
